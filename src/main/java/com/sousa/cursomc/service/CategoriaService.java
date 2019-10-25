0c6e53bd7d07943b35b150bb1fa01bd7aa81f8df
Fabiano Freitas <fabiano.freitas@gmail.com>
2019-10-24 21:09:33 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/sousa/cursomc/service/CategoriaService.java
+++ src/main/java/com/sousa/cursomc/service/CategoriaService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.sousa.cursomc.domain.Categoria;
+import com.sousa.cursomc.exception.ObjectNotFoundException;
 import com.sousa.cursomc.repository.CategoriaRepository;
 
 @Service
@@ -14,6 +15,10 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 		Categoria obj = categoriaRepository.findOne(id);
+		if(obj == null) {
+			throw new ObjectNotFoundException("Objeto nao encontrado ! ID:"+ id 
+					+ "Tipo:" +Categoria.class.getName());
+		}
 		return obj;
 	}
 	
