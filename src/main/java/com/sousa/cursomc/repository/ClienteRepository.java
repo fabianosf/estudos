b6d0199f2dc48ad61c23425223928b18e477ffcc
Fabiano Freitas <fabiano.freitas>
2019-10-28 02:08:26 -0300

    Validacao customizada: email nao repetido na insercao de Cliente

--- src/main/java/com/sousa/cursomc/repository/ClienteRepository.java
+++ src/main/java/com/sousa/cursomc/repository/ClienteRepository.java
@@ -1,11 +1,17 @@
 package com.sousa.cursomc.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
 
 import com.sousa.cursomc.domain.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
+	
+	@Transactional(readOnly = true) //evita lock na transacao de banco de dados
+	Cliente findByEmail(String email);
 
 }
--- src/main/java/com/sousa/cursomc/service/validation/ClienteInsertValidator.java
+++ src/main/java/com/sousa/cursomc/service/validation/ClienteInsertValidator.java
@@ -6,12 +6,19 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.sousa.cursomc.domain.Cliente;
 import com.sousa.cursomc.domain.enums.TipoCliente;
 import com.sousa.cursomc.dto.ClienteNewDTO;
+import com.sousa.cursomc.repository.ClienteRepository;
 import com.sousa.cursomc.resources.exception.FieldMessage;
 import com.sousa.cursomc.service.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
 	
 	@Override
 	public void initialize(ClienteInsert ann) {
@@ -29,6 +36,10 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 			list.add(new FieldMessage("CpfOuCnpj","CNPJ invalido"));
 		}
 		
+		Cliente aux = clienteRepository.findByEmail(objDto.getEmail());
+		if(aux != null) {
+			list.add(new FieldMessage("email", "Email ja existe"));
+		}
 		
 		
 // inclua os testes aqui, inserindo erros na lista
